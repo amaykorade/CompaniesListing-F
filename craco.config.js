872a164ac8e8f54704b428a95278b61d0a8a6e0b
amaykorade <amaykorade5@gmail.com>
2024-11-07 02:01:07 +0530

    Changed craco file and dependencies

--- craco.config.js
+++ craco.config.js
@@ -7,9 +7,21 @@ module.exports = {
             options: {
                 lessLoaderOptions: {
                     lessOptions: {
-                        modifyVars: { '@primary-color': '#1DA57A' }, // Customize theme here
+                        modifyVars: { '@primary-color': '#1DA57A' },
                         javascriptEnabled: true,
                     },
+                },
+                modifyLessRule: function (lessRule, context) {
+                    lessRule.test = /\.less$/;
+                    lessRule.use = [
+                        { loader: 'style-loader' },
+                        { loader: 'css-loader' },
+                        {
+                            loader: 'less-loader',
+                            options: { lessOptions: { javascriptEnabled: true } },
+                        },
+                    ];
+                    return lessRule;
                 },
             },
         },
